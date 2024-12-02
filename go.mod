4a2091e0e06337133aafa7c572a6c610c5a0ccdb
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-02 17:15:32 +0530

    chore: fmt

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goaws
+
+go 1.23.2
--- server.go
+++ server.go
@@ -1,18 +1,17 @@
 package main
 
-
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-		fmt.Println("server up and running...")
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("server up and running...")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("got request...")
-    fmt.Fprintf(w, "Hello, Go AWS")
+	fmt.Println("got request...")
+	fmt.Fprintf(w, "Hello, Go AWS")
 }
