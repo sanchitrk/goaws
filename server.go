c95fb63b6728821c24ebd070bc9cea94387657e1
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-02 17:14:27 +0530

    initial commit

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+		fmt.Println("server up and running...")
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("got request...")
+    fmt.Fprintf(w, "Hello, Go AWS")
+}
