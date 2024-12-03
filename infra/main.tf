9a4f843f0cb391c00b26c1d6857a0c2369f153f5
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-03 18:18:48 +0530

    update terraform script.
    git push shall trigger deployments if gods are in favour.

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,25 @@
-
+# Starts with the provider
+# resources are specific to the provider
+# they are not common across the cloud provider in exact sense
 provider "aws" {
   region = var.aws_region
 }
 
+# Region where we want to deploy the services
 variable "aws_region" {
   description = "The AWS region"
   type        = string
   default     = "ap-southeast-1"
 }
 
+# The stack environment e.g: dev, staging, prod, etc.
 variable "stack_env" {
   description = "The stack environment"
   type        = string
   default     = "staging"
 }
 
+# The deployment stack
 variable "stack_name" {
   description = "The service stack name"
   type        = string
@@ -22,17 +27,21 @@ variable "stack_name" {
 }
 
 
+# The instance SSH key pair, not mandatory but having it anyway
+# Ideally instance should be immutable, ssh discourages it!
 variable "key_pair_name" {
   description = "The Key Pair name"
   type        = string
   default     = "awskp"
 }
 
+# The current AWS identity, pulled from aws config or environment
 data "aws_caller_identity" "current" {}
 
 
 
 # S3 Bucket for CodePipeline artifacts
+# Required for storing build artificats must exist or could be created outside
 resource "aws_s3_bucket" "artifacts" {
   bucket = "${var.stack_name}-${var.stack_env}-${var.aws_region}-codepipeline-${data.aws_caller_identity.current.account_id}"
 
@@ -65,6 +74,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_buck
 }
 
 # Block public access
+# Dont want to make the S3 public with all the built source codes yakes!
 resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_public_access_block" {
   bucket = aws_s3_bucket.artifacts.id
 
@@ -125,6 +135,7 @@ resource "aws_iam_role" "codebuild_role" {
 }
 
 
+# AWS IAM role for code deployment for the stack
 resource "aws_iam_role" "codedeploy_role" {
   name = "${var.stack_name}-codedeploy-role"
 
@@ -147,6 +158,8 @@ resource "aws_iam_role" "codedeploy_role" {
   }
 }
 
+
+# AWS IAM role for code pipeline for this stack.
 resource "aws_iam_role" "codepipeline_role" {
   name = "${var.stack_name}-codepipeline-role"
 
@@ -169,13 +182,7 @@ resource "aws_iam_role" "codepipeline_role" {
   }
 }
 
-
-resource "aws_codestarconnections_connection" "github" {
-  name          = "${var.github_repo}-${var.github_branch}"
-  provider_type = "GitHub"
-}
-
-
+# AWS IAM role for codebuild for the stack
 resource "aws_iam_role_policy" "codebuild_policy" {
   name = "${var.stack_name}-codebuild-policy"
   role = aws_iam_role.codebuild_role.id
@@ -208,7 +215,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
   })
 }
 
-
+# Now that we have policy created - attach to specific roles
 resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
   role       = aws_iam_role.codedeploy_role.name
@@ -267,7 +274,7 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
   })
 }
 
-
+# AWS codebuild project specifies build configuration
 resource "aws_codebuild_project" "app" {
   name         = "${var.stack_name}-${var.stack_env}-build"
   description  = "Build project for ${var.stack_name} ${var.stack_env} application"
@@ -299,7 +306,6 @@ resource "aws_codedeploy_app" "app" {
   name = "${var.stack_name}-${var.stack_env}"
 }
 
-
 resource "aws_codedeploy_deployment_group" "app" {
   app_name              = aws_codedeploy_app.app.name
   deployment_group_name = "${var.stack_name}-${var.stack_env}-deployment-group"
@@ -318,7 +324,6 @@ resource "aws_codedeploy_deployment_group" "app" {
     }
   }
 }
-
 
 resource "aws_codepipeline" "pipeline" {
   name     = "${var.stack_name}-${var.stack_env}-pipeline"
@@ -538,3 +543,62 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.ec2_role.name
 }
+
+# AWS codestar connection - might have to authenticate in console for permissions.
+resource "aws_codestarconnections_connection" "github" {
+  name          = "${var.github_repo}-${var.github_branch}"
+  provider_type = "GitHub"
+}
+
+resource "aws_codepipeline_webhook" "github_webhook" {
+  name            = "${var.stack_name}-${var.stack_env}-webhook"
+  authentication  = "GITHUB_HMAC"
+
+  target_action   = "Source"
+  target_pipeline = aws_codepipeline.pipeline.name
+
+  authentication_configuration {
+    secret_token = var.github_token
+  }
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+
+  tags = {
+    Environment = var.stack_env
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "github_repository_webhook" "github_webhook" {
+  repository = split("/", var.github_repo)[1]
+  
+  configuration {
+    url          = aws_codepipeline_webhook.github_webhook.url
+    content_type = "json"
+    insecure_ssl = false
+    secret       = var.github_token
+
+  }
+
+  events = ["push"]
+}
+
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+provider "github" {
+  token = var.github_token
+  owner = split("/", var.github_repo)[0]
+}
+
