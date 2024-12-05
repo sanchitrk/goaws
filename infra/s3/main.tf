fdfe0457f7df2107ad8332e736ddb4e39cc7ec61
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-05 18:37:53 +0530

    dont auto deploy

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,50 +1,61 @@
-# Provider and data source configuration
+
+# Starts with the provider
+# resources are specific to the provider
+# they are not common across the cloud provider in exact sense
 provider "aws" {
   region = var.aws_region
 }
 
+# Region where we want to deploy the services
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+# The stack environment e.g: dev, staging, prod, etc.
+variable "stack_env" {
+  description = "The stack environment"
+  type        = string
+  default     = "staging"
+}
+
+# The deployment stack
+variable "stack_name" {
+  description = "The service stack name"
+  type        = string
+  default     = "srv"
+}
+
 data "aws_caller_identity" "current" {}
 
-# Variables
-variable "stack_name" {
-  type        = string
-  description = "Name of the stack"
-}
 
-variable "stack_env" {
-  type        = string
-  description = "Environment (e.g., dev, prod, staging)"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region where resources will be created"
-}
-
-# S3 Bucket for CodePipeline artifacts
-resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "${var.stack_name}-${var.stack_env}-${var.aws_region}-codepipeline-${data.aws_caller_identity.current.account_id}"
+# S3 Bucket for artifacts
+# Required for storing build artificats must exist or could be created outside
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${var.stack_name}-${var.stack_env}-${var.aws_region}-artifacts-${data.aws_caller_identity.current.account_id}"
 
   force_destroy = true # Enable if you want to delete the bucket even if it contains objects
 
   tags = {
-    Name        = "${var.stack_name}-codepipeline-bucket"
+    Name        = "${var.stack_name}-artifacts-bucket"
     Environment = var.stack_env
     ManagedBy   = "terraform"
   }
 }
 
 # Bucket versioning
-resource "aws_s3_bucket_versioning" "codepipeline_bucket_versioning" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
+resource "aws_s3_bucket_versioning" "artifacts_versioning" {
+  bucket = aws_s3_bucket.artifacts.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+
 # Server-side encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_bucket_encryption" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts_bucket_encryption" {
+  bucket = aws_s3_bucket.artifacts.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -53,9 +64,24 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_buck
   }
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "artifacts_lifecycle" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    id     = "cleanup-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
+
 # Block public access
-resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_public_access_block" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
+# Dont want to make the S3 public with all the built source codes yakes!
+resource "aws_s3_bucket_public_access_block" "artifacts_bucket_public_access_block" {
+  bucket = aws_s3_bucket.artifacts.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -64,10 +90,11 @@ resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_public_access_
 }
 
 # Output the bucket name
-output "codepipeline_bucket_name" {
-  value = aws_s3_bucket.codepipeline_bucket.id
+output "artifacts_bucket_name" {
+  value = aws_s3_bucket.artifacts.id
 }
 
-output "codepipeline_bucket_arn" {
-  value = aws_s3_bucket.codepipeline_bucket.arn
+# Output the bucket ARN
+output "artifacts_bucket_arn" {
+  value = aws_s3_bucket.artifacts.arn
 }
