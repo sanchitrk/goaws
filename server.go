9d7b9e6a7a6d5218edbd8174e9f84ca1aebbb96b
Sanchit Rk <sanchitrrk@gmail.com>
2024-12-05 22:49:34 +0530

    check if alb deploys

--- server.go
+++ server.go
@@ -13,5 +13,5 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("got request...")
-	fmt.Fprintf(w, "Hello, Go AWS!")
+	fmt.Fprintf(w, "Hello, Go AWS Deployed App!")
 }
